8c7072218e9791efd161078346594f7d6ba6f40a
shakenrobot <53802985+shakenrobot@users.noreply.github.com>
2020-10-25 16:42:56 +1300

    Add files via upload

---
+++ CS235Flix/domain/Assignment2init.py
@@ -0,0 +1,11 @@
+from CS235Flix.datafilereaders.movie_file_csv_reader import MovieFileCSVReader
+
+
+def main():
+    filename = 'datafiles/Data1000Movies.csv'
+    movie_file_reader = MovieFileCSVReader(filename)
+    movie_file_reader.read_csv_file()
+
+
+if __name__ == "__main__":
+    main()
